56cc4add1c87f1130ec55abab03a8f75de596526
KaterynaChechotka <112935863+KaterynaChechotka@users.noreply.github.com>
2023-09-23 15:02:34 +0300

    Add files via upload

--- module4-assignment/javascript/script.js
+++ module4-assignment/javascript/script.js
@@ -1,1 +1,10 @@
+(function mynames() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+var firstLetter = names[i].charAt(0).toLowerCase();
+if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+})();
